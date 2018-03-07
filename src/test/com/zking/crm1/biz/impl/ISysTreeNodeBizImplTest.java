f76c44b517446404e25560f89afabcd042d94b0b
xt1575667942 <1575667942@qq.com>
2018-03-08 00:43:20 +0800

    搭建操作

---
+++ src/test/com/zking/crm1/biz/impl/ISysTreeNodeBizImplTest.java
@@ -0,0 +1,34 @@
+package com.zking.crm1.biz.impl;
+
+import com.zking.crm1.biz.ISysTreeNodeBiz;
+import com.zking.crm1.model.SysTreeNode;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ISysTreeNodeBizImplTest extends BaseTest{
+
+
+    @Autowired
+    private ISysTreeNodeBiz sysTreeNodeBiz;
+
+    private SysTreeNode sysTreeNode;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        sysTreeNode = new SysTreeNode();
+    }
+
+    @Test
+    public void listSysTreeNode() throws Exception {
+        List<SysTreeNode> list = sysTreeNodeBiz.listSysTreeNode(sysTreeNode);
+        for(SysTreeNode d:list){
+            System.out.println(d);
+        }
+    }
+
+}
