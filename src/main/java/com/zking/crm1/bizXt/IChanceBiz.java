98f31345f27121f63cfcaf3b6d791adb59bd9131
xt1575667942 <1575667942@qq.com>
2018-03-10 19:11:36 +0800

    修改操作

---
+++ src/main/java/com/zking/crm1/bizXt/IChanceBiz.java
@@ -0,0 +1,36 @@
+package com.zking.crm1.bizXt;
+
+import com.zking.crm1.model.Chance;
+import com.zking.crm1.util.PageBean;
+
+import java.util.List;
+
+public interface IChanceBiz {
+
+
+    /**
+     * 显示所有
+     */
+    List<Chance> listChance(Chance chance,PageBean pageBean);
+
+    /**
+     * 删除
+     */
+    void delChance(Chance chance);
+
+    /**
+     * 新增方法
+     */
+    void addChance(Chance chance);
+
+    /**
+     * 查单个的方法
+     */
+    Chance loadChance(Chance chance);
+
+    /**
+     * 指派方法
+     */
+    void editChanceStaus(Chance chance);
+
+}
---
+++ src/main/java/com/zking/crm1/bizXt/impl/IChanceBizImpl.java
@@ -0,0 +1,41 @@
+package com.zking.crm1.bizXt.impl;
+
+import com.zking.crm1.bizXt.IChanceBiz;
+import com.zking.crm1.mapper.ChanceMapper;
+import com.zking.crm1.model.Chance;
+import com.zking.crm1.util.PageBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IChanceBizImpl implements IChanceBiz {
+
+    @Autowired
+    private ChanceMapper chanceMapper;
+    @Override
+    public List<Chance> listChance(Chance chance,PageBean pageBean) {
+        return chanceMapper.listchance(chance);
+    }
+
+    @Override
+    public void delChance(Chance chance) {
+        chanceMapper.deleteByPrimaryKey(chance.getChcId());
+    }
+
+    @Override
+    public void addChance(Chance chance) {
+        chanceMapper.insertSelective(chance);
+    }
+
+    @Override
+    public Chance loadChance(Chance chance) {
+        return chanceMapper.selectByPrimaryKey(chance.getChcId());
+    }
+
+    @Override
+    public void editChanceStaus(Chance chance) {
+         chanceMapper.editChanceStats(chance);
+    }
+}
